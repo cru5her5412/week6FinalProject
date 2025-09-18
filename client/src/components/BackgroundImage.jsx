b19f219e17a1da7784f357946d3254c7f17b0faf
Callum James Lodge <callumjlodge@gmail.com>
2025-09-18 15:05:15 +0100

    MVP accomplished

--- client/src/components/BackgroundImage.jsx
+++ client/src/components/BackgroundImage.jsx
@@ -2,8 +2,8 @@ export default function BackgroundImage({ index, imageData }) {
   let currentImage = imageData[index]?.url;
   let currentImageAlt = imageData[index]?.alt;
   return (
-    <>
+    <div className="w-screen flex justify-center">
       <img className="h-screen" src={currentImage} alt={currentImageAlt} />
-    </>
+    </div>
   );
 }
