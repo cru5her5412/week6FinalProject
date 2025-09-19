b02d1c3b4c635909993de08c169b0b66dca2aea2
Callum James Lodge <callumjlodge@gmail.com>
2025-09-19 13:42:55 +0100

    fresh commit for recreation of server on render

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
-app.use("./images", express.static("images"));
+app.use("images", express.static("images"));
 app.listen(8080, () => console.log("listening on port 8080"));
 app.get("/", (request, response) => {
   response = "hi";
